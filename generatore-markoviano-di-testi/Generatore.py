f9de85071b6d23774bb9730adbf8c0cbf88a22b7
riky.fidanza <riky.fidanza@gmail.com>
2024-07-03 16:06:30 +0200

    rilevamento e manipolazione del testo

--- generatore-markoviano-di-testi/Generatore.py
+++ generatore-markoviano-di-testi/Generatore.py
@@ -1,4 +1,17 @@
+import re
 
+class Generatore:
 
+    def __init__(self, filename:str):
+        testo = ""
+        with open(filename, 'r') as file:
+            testo = file.read()        
+            pattern = r'[^\w\s]'           
+            testo = re.sub(pattern, '', testo)
+        
+        pattern = r'[ \n]'
+        self.d = {}    
+        parole = re.split(pattern, testo)
+        print(parole)
 
-    
+g = Generatore('testo.txt') 
