76818ef1274e6e2beb328b4219009897fdcdacb9
riky.fidanza <riky.fidanza@gmail.com>
2024-07-03 16:38:34 +0200

    generatore finito

--- generatore-markoviano-di-testi/Generatore.py
+++ generatore-markoviano-di-testi/Generatore.py
@@ -1,17 +1,47 @@
+import random
 import re
 
 class Generatore:
 
     def __init__(self, filename:str):
         testo = ""
-        with open(filename, 'r') as file:
-            testo = file.read()        
-            pattern = r'[^\w\s]'           
-            testo = re.sub(pattern, '', testo)
-        
-        pattern = r'[ \n]'
-        self.d = {}    
-        parole = re.split(pattern, testo)
-        print(parole)
 
+        with open(filename, 'r') as file:
+            testo = file.read()   
+
+        pattern = r'[^\w\s]'           
+        testo = re.sub(pattern, '', testo)
+        testo = testo.lower()
+        
+        pattern = r'[ \n]' 
+        parole = re.split(pattern, testo)
+
+        self.d = {}   
+
+        for i in range(0, len(parole)-1):
+            if parole[i] in self.d:
+                self.d[parole[i]].append(parole[i+1])
+            else: 
+                self.d[parole[i]] = [parole[i+1]]
+    
+    def genera_testo(self, start:str, len_testo):
+        testo = ""
+        for i in range(0, len_testo-1):
+            scelta = random.choice(self.d[start])
+            testo += scelta + " "
+            start = scelta
+        scelta = random.choice(self.d[start])
+        testo += scelta
+        return testo            
+
+                
+        
+
+
+        
+
+
+        
 g = Generatore('testo.txt') 
+testo = g.genera_testo("il",100)
+print(testo)
